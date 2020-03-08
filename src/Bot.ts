2382356f2d43457175e4157669295b8829e8ab26
Emmanuel Tierra <emman.tierra@gmail.com>
2020-03-08 15:30:23 +0800

    Add listener for messages

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,5 +1,5 @@
 import { injectable } from "inversify";
-import { Client } from "discord.js";
+import { Client, Message, TextChannel } from "discord.js";
 
 @injectable()
 export class Bot
@@ -11,8 +11,19 @@ export class Bot
     this.client = client;
   }
 
-  public listen(token: any) : Promise<string>
+  public listen(token: any, relay_ch_id: any, confession_ch_id: any) : Promise<string>
   {
+    this.client.on('message', (message: Message) => {
+      if (message.author.bot) {
+        return;
+      }
+
+      if (message.channel.type == "dm") {
+        let channel = this.client.channels.get(relay_ch_id) as TextChannel;
+        channel.send(message.content);
+      }
+    });
+
     return this.client.login(token);
   }
 }
