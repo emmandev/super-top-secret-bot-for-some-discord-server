2a975464bae401940ca629228262f45583f19eb7
Emmanuel Tierra <emman.tierra@gmail.com>
2020-03-08 15:28:14 +0800

    Add Bot to AppContainer

--- src/providers/container.ts
+++ src/providers/container.ts
@@ -1,8 +1,10 @@
 import { Container } from "../../node_modules/inversify";
 import { Client } from "discord.js";
+import { Bot } from "../Bot";
 
 let AppContainer = new Container();
 
+AppContainer.bind<Bot>(Bot).toSelf();
 AppContainer.bind<Client>(Client).toConstantValue(new Client());
 
 export default AppContainer;
